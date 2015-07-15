f3113d73d8d1b5804e75189e9e8c5b8abf686cd4
kzaiter <kzaiter@aol.com>
2015-07-15 14:06:30 -0400

    Create MEM_WB_Read.java

---
+++ MEM_WB_Read.java
@@ -0,0 +1,17 @@
+// MEM/WB_Read (read  by the WB stage)      
+public class MEM_WB_Read
+{
+	public byte RegDst;
+	public byte ALUSrc;
+	public byte ALUOp1;
+	public byte ALUOp2;
+	public byte MemRead;
+	public byte MemWrite;
+	public byte Branch;
+	public byte MemToReg;
+	public byte RegWrite;
+
+	public int ALUResult;
+	public int WriteRegNum;
+	public int LWDataValue;
+}
