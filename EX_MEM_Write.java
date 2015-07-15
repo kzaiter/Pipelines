2dcda0f66724a77a3907365999228f695f566498
kzaiter <kzaiter@aol.com>
2015-07-15 14:02:07 -0400

    Create EX_MEM_Write.java

---
+++ EX_MEM_Write.java
@@ -0,0 +1,17 @@
+public class EX_MEM_Write
+{
+	public byte RegDst;
+	public byte ALUSrc;
+	public byte ALUOp1;
+	public byte ALUOp2;
+	public byte MemRead;
+	public byte MemWrite;
+	public byte Branch;
+	public byte MemToReg;
+	public byte RegWrite;
+	public int CalcBTA;
+	public boolean Zero;
+	public int ALUResult;
+	public int SWValue;
+	public int WriteRegNum;
+}
