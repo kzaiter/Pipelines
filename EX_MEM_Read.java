d1ad14a4edf97d2d118827753d5710e7669f96b7
kzaiter <kzaiter@aol.com>
2015-07-15 13:58:00 -0400

    Create EX_MEM_Read.java

---
+++ EX_MEM_Read.java
@@ -0,0 +1,19 @@
+
+public class EX_MEM_Read
+{
+	public byte RegDst;
+	public byte ALUSrc;
+	public byte ALUOp1;
+	public byte ALUOp2;
+	public byte MemRead;
+	public byte MemWrite;
+	public byte Branch;
+	public byte MemToReg;
+	public byte RegWrite;
+	public int CalcBTA;
+	public boolean Zero;
+	public int ALUResult;
+	public int WriteRegNum;
+	public int SWValue;
+
+}
