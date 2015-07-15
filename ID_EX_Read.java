cf0f242678c51f5ee6f1d718968bf0fa24d79435
kzaiter <kzaiter@aol.com>
2015-07-15 14:02:53 -0400

    Create ID_EX_Read.java

---
+++ ID_EX_Read.java
@@ -0,0 +1,19 @@
+public class ID_EX_Read
+{
+	public byte RegDst;
+	public byte ALUSrc;
+	public byte ALUOp1;
+	public byte ALUOp2;
+	public byte MemRead;
+	public byte MemWrite;
+	public byte Branch;
+	public byte MemToReg;
+	public byte RegWrite;
+	public int IncrPC;
+	public int ReadReg1Value;
+	public int ReadReg2Value;
+	public int WriteRegNum;
+	public int SEOffset;
+	public int Function;
+	public boolean Rtype;
+}
