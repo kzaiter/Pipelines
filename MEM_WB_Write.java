28792d71189d6841182aa64a0258d35c905dd42c
kzaiter <kzaiter@aol.com>
2015-07-15 14:06:54 -0400

    Create MEM_WB_Write.java

---
+++ MEM_WB_Write.java
@@ -0,0 +1,18 @@
+      
+public class MEM_WB_Write
+{
+	public byte RegDst;
+	public byte ALUSrc;
+	public byte ALUOp1;
+	public byte ALUOp2;
+	public byte MemRead;
+	public byte MemWrite;
+	public byte Branch;
+	public byte MemToReg;
+	public byte RegWrite;
+
+	public int ALUResult;
+	public int WriteRegNum;
+	public int LWDataValue;
+
+}
